3eaf64745e12a52a06929dd3cf708c6b59bf9667
noy2 <gameplay3542@icloud.com>
2022-10-21 17:37:54 +0900

    Add: Component Button

---
+++ myfolio/src/components/Button.js
@@ -0,0 +1,63 @@
+import styled from "@emotion/styled";
+import { css } from "@emotion/react";
+import React from "react";
+import * as colors from "../styles/colors";
+
+const SIZES = {
+  sm: css`
+    --button-height: 32px;
+    --button-radius: 4px;
+    --button-font-size: 12px;
+    --button-padding: 6px 12px;
+  `,
+  md: css`
+    --button-height: 40px;
+    --button-radius: 4px;
+    --button-font-size: 16px;
+    --button-padding: 8px 16px;
+  `,
+  lg: css`
+    --button-height: 48px;
+    --button-radius: 6px;
+    --button-font-size: 20px;
+    --button-padding: 8px 20px;
+  `,
+};
+
+export default function Button({ disabled, children, size }) {
+  const sizeStyle = SIZES[size];
+
+  return (
+    <StyledButton disabled={disabled} sizeStyle={sizeStyle}>
+      {children}
+    </StyledButton>
+  );
+}
+
+const StyledButton = styled.button`
+  ${(p) => p.sizeStyle}
+
+  height: var(--button-height, 40px);
+  margin: 0;
+  border: none;
+  border-radius: var(--button-radius, 4px);
+  cursor: pointer;
+  padding: var(--button-padding, 8px 16px);
+  background-color: ${colors.primary500};
+  color: ${colors.white};
+
+  font-family: "SUIT";
+  font-size: var(--button-font-size, 16px);
+  font-weight: 600;
+
+  &:hover {
+    background-color: ${colors.primary700};
+  }
+  &:active {
+    background-color: ${colors.primary900};
+  }
+  &:disabled {
+    cursor: default;
+    background-color: ${colors.grey100};
+  }
+`;
