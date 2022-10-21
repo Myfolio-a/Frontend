62862925f03de603bfb096de0e7c5085c0db7b3e
noy2 <gameplay3542@icloud.com>
2022-10-21 16:58:57 +0900

    Add: Color scales

---
+++ myfolio/src/styles/colors.js
@@ -0,0 +1,48 @@
+// Grey scale
+export const white = "white";
+export const grey50 = "#f1f1f1";
+export const grey100 = "#DDDDDD";
+export const grey300 = "#BDBDBD";
+export const grey400 = "#98989E";
+export const grey500 = "#62626A";
+export const grey600 = "#202529";
+export const grey900 = "#2C2C2C";
+export const grey1000 = "#000000";
+
+// Primary scale
+export const primary20 = "#F7F0FF";
+export const primary50 = "#F2E5FF";
+export const primary100 = "#EFCCFF";
+export const primary200 = "#DB99FF";
+export const primary300 = "#C166FF";
+export const primary500 = "#7F00FF";
+export const primary700 = "#4900B7";
+export const primary900 = "#24007A";
+
+// info color
+export const info100 = "#CCFEFE";
+export const info300 = "#66EAFE";
+export const info500 = "#02B9FC";
+export const info700 = "#016CB5";
+export const info900 = "#003778";
+
+// Success color
+export const success100 = "#E6FDCE";
+export const success300 = "#A0F46E";
+export const success500 = "#41DD13";
+export const success700 = "#159F09";
+export const success900 = "#036A09";
+
+// Warning color
+export const warning100 = "#FFF9D2";
+export const warning300 = "#FFE679";
+export const warning500 = "#FFCC21";
+export const warning700 = "#B78910";
+export const warning900 = "#7A5406";
+
+// Error color
+export const error100 = "#FFE8CE";
+export const error300 = "#FFA86C";
+export const error500 = "#FF4D0B";
+export const error700 = "#B71D05";
+export const error900 = "#7A0202";
