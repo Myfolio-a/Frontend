4d18368a3629e0438edb8030d8b46d4fdc5c02cf
noy2 <gameplay3542@icloud.com>
2022-10-23 02:59:07 +0900

    Modify: added icon in input, some styles modified

--- myfolio/src/components/Input.js
+++ myfolio/src/components/Input.js
@@ -2,6 +2,7 @@ import styled from "@emotion/styled";
 import { css } from "@emotion/react";
 import React from "react";
 import * as colors from "../styles/colors";
+import { HiBolt } from "react-icons/hi2";
 
 const VARIANTS = {
   error: css`
@@ -19,23 +20,64 @@ export default function Input({
   value,
   label,
   LeftDescription,
+  icon,
 }) {
   const variantStyle = VARIANTS[variant];
   return (
     <InputFrame>
       <InputLabel>{label}</InputLabel>
-      <StyledInput
-        variantStyle={variantStyle}
-        placeholder={placeholder}
-        disabled={disabled}
-        value={value}
-      />
+      <StyledInputFrame variantStyle={variantStyle} disabled={disabled}>
+        <StyledInput
+          variantStyle={variantStyle}
+          placeholder={placeholder}
+          disabled={disabled}
+          value={value}
+        />
+        <HiBolt
+          style={{ width: "16px", height: "16px", margin: "0 12px 0 0" }}
+        />
+      </StyledInputFrame>
       <InputLeftDescription variantStyle={variantStyle}>
         {LeftDescription}
       </InputLeftDescription>
     </InputFrame>
   );
 }
+
+const StyledInputFrame = styled.div`
+  ${(p) => p.variantStyle}
+  ${(p) =>
+    p.disabled &&
+    css`
+      pointer-events: none;
+    `};
+
+  height: 36px;
+  display: flex;
+  align-items: center;
+  gap: 8px;
+  padding: 2px;
+  margin: 0;
+  border: none;
+  outline: none;
+  border-radius: 4px;
+  box-shadow: var(
+    --input-default-border,
+    inset 0px 0px 0px 1px ${colors.grey100}
+  );
+  &:hover {
+    box-shadow: var(
+      --input-hover-border,
+      inset 0px 0px 0px 1px ${colors.grey900}
+    );
+  }
+  &:focus-within {
+    box-shadow: var(
+      --input-focus-border,
+      inset 0px 0px 0px 2px ${colors.primary500}
+    );
+  }
+`;
 
 const InputFrame = styled.div`
   display: flex;
@@ -62,37 +104,20 @@ const InputLabel = styled.div`
 const StyledInput = styled.input`
   ${(p) => p.variantStyle}
 
+  border-radius: 2.5px;
   height: 24px;
   margin: 0;
   border: none;
   outline: none;
-  box-shadow: var(
-    --input-default-border,
-    inset 0px 0px 0px 1px ${colors.grey100}
-  );
-  border-radius: 4px;
-  padding: var(--input-padding, 8px 12px);
+
+  padding: var(--input-padding, 6px 12px);
   background-color: ${colors.white};
 
   font-family: "SUIT";
   font-size: 14px;
   font-weight: 400;
 
-  &:hover {
-    box-shadow: var(
-      --input-hover-border,
-      inset 0px 0px 0px 1px ${colors.grey900}
-    );
-  }
-  &:focus {
-    box-shadow: var(
-      --input-focus-border,
-      inset 0px 0px 0px 2px ${colors.primary500}
-    );
-  }
   &:disabled {
-    cursor: default;
-    box-shadow: inset 0px 0px 0px 1px ${colors.grey50};
     color: ${colors.grey300};
 
     &::placeholder {
