291a8962a5521e0bdc62e86e00cc1ddd39f63398
noy2 <gameplay3542@icloud.com>
2022-12-13 18:56:04 +0900

    Modify: for adding Tab component

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -1,3 +1,39 @@
+import styled from "@emotion/styled";
+import GlobalNavigation from "../components/GlobalNavigation";
+import Tab from "../components/Tab";
+import * as colors from "../styles/colors";
+
 export default function Edit() {
-  return <div>EDIT</div>;
+  return (
+    <MainFrame>
+      <GlobalNavigation />
+      <EditHomeFrame>
+        <Title>Resumes & Portfolios</Title>
+        <Tab />
+      </EditHomeFrame>
+    </MainFrame>
+  );
 }
+
+const Title = styled.div`
+  font-size: 28px;
+  font-weight: 600;
+  line-height: 36px;
+  color: ${colors.grey900};
+
+  margin-top: 32px;
+`;
+
+const EditHomeFrame = styled.div`
+  width: 960px;
+  height: 100vh;
+
+  padding: 32px;
+`;
+
+const MainFrame = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  margin-left: 240px;
+`;
