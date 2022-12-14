37d83635fa0d0cbc26930eab927187411a3f112e
noy2 <gameplay3542@icloud.com>
2022-12-15 03:27:26 +0900

    Add: added file for code

---
+++ src/components/PortfolioGridItem.js
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+import * as colors from "../styles/colors";
+
+export default function PortfolioGridItem() {
+  return <MainFrame>PortfolioGridItem</MainFrame>;
+}
+
+const MainFrame = styled.div`
+  background-color: red;
+  height: 270px;
+`;
