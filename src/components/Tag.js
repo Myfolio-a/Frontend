dcf3713199ad1f8ebe8d4189544940a4a0937fc3
noy2 <gameplay3542@icloud.com>
2023-01-18 21:24:16 +0900

    Add: added component for tag

---
+++ src/components/Tag.js
@@ -0,0 +1,17 @@
+import styled from "@emotion/styled";
+import * as colors from "../styles/colors";
+
+export default function Tag({ children }) {
+  return <MainFrame>{children}</MainFrame>;
+}
+
+const MainFrame = styled.div`
+  padding: 1px 6px;
+  border-radius: 4px;
+
+  background-color: ${colors.white};
+  color: ${colors.grey900};
+
+  font-size: 14px;
+  font-weight: 700;
+`;
